bc7e4495d60715241598f92130b7523c00ea30a4
ts2013 <tsegai@usa.net>
2022-12-04 04:46:08 -0800

    more data fields added

--- resources/views/contactemail/email.blade.php
+++ resources/views/contactemail/email.blade.php
@@ -1,14 +1,36 @@
 @component('mail::message')
     # Introduction
 
-    New message.
-    There is a message from {{ $first_name }} {{ $last_name }}
+    This is a message from {{  $data['first_name'] }} {{  $data['last_name'] }}
+   
+    Email:
+     {{  $data['email'] }}
+
+    Address:
+     {{  $data['address'] }} 
+     {{  $data['city'] }}
+     {{  $data['state'] }}
+     {{  $data['zip_code'] }}
+
+    Phone:
+     {{  $data['phone'] }} 
 
     Message:
-    {{ $message }}
+     {{ $data['message'] }}
 
-    Price Range:
-    from ${{ $price_range_from }} to ${{ $price_range_to }}
+    Looking for price range:
+     from ${{ $data['price_range_from'] }} to ${{ $data['price_range_to'] }}
+
+    Type of property:
+     {{ $data['type_of_property'] }}
+
+    Number of bedrooms:
+     {{ $data['number_of_bedrooms'] }}
+
+    Number of bathrooms:
+     {{ $data['number_of_bathrooms'] }}
+
+
 
     Thanks,<br>
     {{ config('app.name') }}
